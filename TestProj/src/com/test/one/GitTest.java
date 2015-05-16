470992c5ef86d372448f22a3836cae7fa80a5ffc
jzkhan <jzkhan44@gmail.com>
2015-05-16 14:05:22 +0100

    ignore files

---
+++ TestProj/src/com/test/one/GitTest.java
@@ -0,0 +1,19 @@
+package com.test.one;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int i =0; i <=3; i++){
+			
+			System.out.print("Hello world!");
+			
+			
+		}
+		
+
+	}
+
+}
